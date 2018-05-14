3cb16950eb63a71f1fa70767ec4d8994167d83b9
victoryboby <39250437+victoryboby@users.noreply.github.com>
2018-05-14 10:42:58 +0800

    test

---
+++ lostAndFounds/lib/string.func.php
@@ -0,0 +1,19 @@
+<?php
+//$type验证码类型
+//$length验证码长度
+function bulidRandomString($type = 1,$length = 4)
+{
+    
+    if ($type == 1) {
+        $chars = join("", range(0, 9));
+    } elseif ($type == 2) {
+        $chars = join("", array_merge(range("a", "z"), range("A", "Z")));
+    } elseif ($type == 3) {
+        $chars = join("", array_merge(range("a", "z"), range("A", "Z"), range(0, 9)));
+    }
+    if ($length > strlen($chars)) {
+        exit("字符串长度不够");
+    }
+    $chars = str_shuffle($chars);
+    return substr($chars, 0, $length);
+}
